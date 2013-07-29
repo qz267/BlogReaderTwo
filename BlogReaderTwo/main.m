c672639f304762d9a53027221da436191c21f316
Zheng Qin <kevin.qin.cn@gmail.com>
2013-07-29 16:41:39 -0400

    Initial Commit

---
+++ BlogReaderTwo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReaderTwo
+//
+//  Created by Zheng Qin on 7/29/13.
+//  Copyright (c) 2013 Zheng Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
