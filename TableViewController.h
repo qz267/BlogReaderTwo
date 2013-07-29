79205e95449e81438acbb218cc24fc5e1f8d95da
Zheng Qin <kevin.qin.cn@gmail.com>
2013-07-29 17:09:40 -0400

    Update table view controller

---
+++ TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  BlogReaderTwo
+//
+//  Created by Zheng Qin on 7/29/13.
+//  Copyright (c) 2013 Zheng Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *titles;
+
+@end
